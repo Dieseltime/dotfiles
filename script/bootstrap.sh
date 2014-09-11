8dee199c047f3a8866d50b5d6f8a23f63340976e
Scott Van Hess <vanhess@gmail.com>
2014-09-11 11:19:04 -0700

    :running_shirt_with_sash:

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/env bash
 
-export ARCH=$(uname -s)
-case "$ARCH" in
-  'Darwin') export MAC_ONLY=true ;;
-  *)        export MAC_ONLY=false ;;
-esac
-
-if $PERSONAL ; then
-  which -s brew
-  if [[ $? != 0 ]] ; then
-    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-  else
-    brew update
-  fi
-fi
-
 dotfiles_profile="${HOME}/.dotfiles/bash/.profile.sh"
 
 bootstrap () {
@@ -26,12 +11,14 @@ bootstrap () {
   ruby $HOME/.dotfiles/atom.symlink/install.rb
 }
 
+# Load Functions
 for functions in `find $HOME/.dotfiles -maxdepth 2 -name functions.sh`
 do
   echo "loading ${functions}"
   source $functions
 done
 
+# Append source files location to .profile.sh
 for functions in `find ${HOME}/.dotfiles -maxdepth 2 -name functions.sh`
 do
   if ! grep -q "[[ -s \"\$HOME/${functions}\" ]] && source \"\$HOME/${functions}\"" "${dotfiles_profile}" ; then
