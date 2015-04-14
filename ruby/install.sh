984eba1fe294768b36259dad81d891ee31d8c29e
Scott Van Hess <svanhess@g2llc.com>
2015-04-14 14:40:39 -0700

    :loudspeaker:

--- ruby/install.sh
+++ ruby/install.sh
@@ -1,7 +1,8 @@
 # Install ruby
-
-sudo apt-get update
-sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties
+if [ "$(uname -s)" == "Linux" ]; then
+  sudo apt-get update
+  sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties
+fi
 
 git clone git://github.com/sstephenson/rbenv.git .rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
