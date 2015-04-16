f3d9302b9d92374eccad53b3b8707482f6b94eab
Susie Van Hess <susie@Susies-MacBook-Pro.local>
2015-04-16 16:47:26 -0700

    :zzz:

--- bash/install.sh
+++ bash/install.sh
@@ -12,7 +12,7 @@ fi
 touch "$HOME/.hushlogin"
 
 if ! which brew >/dev/null 2>&1; then
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" </dev/null
 fi
 
 if [ "$(uname -s)" == "Darwin" ]
