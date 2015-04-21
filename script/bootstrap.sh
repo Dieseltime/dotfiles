e09f70ad88ac96388dcc4794ef14a40a98490946
Scott Van Hess <vanhess@gmail.com>
2015-04-21 01:11:20 -0700

    :us:

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -7,14 +7,17 @@ touch "$HOME/.bash_profile"
 # Load Functions
 for functions in `find $HOME/.dotfiles -maxdepth 2 -name functions.sh`
 do
+  echo "Loading $functions"
   source $functions
 done
 
+echo "Loading .bash_profile"
 source $HOME/.dotfiles/bash/.profile.sh
 
 # Run installers
 for installer in `find $HOME/.dotfiles -maxdepth 2 -name install.sh`
 do
+  echo "Installing $installer"
   source $installer
 done
 
