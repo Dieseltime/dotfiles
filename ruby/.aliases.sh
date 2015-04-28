0f812b5fa8d712d72144b95af90ac94102351b1b
Scott Van Hess <vanhess@gmail.com>
2015-04-28 16:55:22 -0700

    :anchor:

--- ruby/.aliases.sh
+++ ruby/.aliases.sh
@@ -1,2 +1,4 @@
-alias pstart="passenger start $@"
-alias pstop="passenger stop $@"
+alias pstart="start-passenger"
+alias passenger-start="start-passenger"
+alias pstop="stop-passenger"
+alias passenger-stop="stop-passenger"
